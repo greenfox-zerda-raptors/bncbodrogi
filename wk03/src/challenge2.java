83bc4969022f191bc658260c9a83d43ac305e0eb
BBENCE <bncbodrogi@gmail.com>
2016-11-03 22:27:42 +0100

    challenge 2 done

---
+++ wk03/src/challenge2.java
@@ -0,0 +1,22 @@
+
+public class challenge2 {
+    public static void main(String[] args) {
+
+        //Loop printing out prime numbers from 2 to 100
+
+        int i=100;
+
+        while (i != 1){
+            int y = i - 1;
+            while (i%y!=0 && y !=1){
+                y--;
+            }
+            if (y == 1){
+                System.out.println(i);
+            }
+            i--;
+        }
+
+
+    }
+}
