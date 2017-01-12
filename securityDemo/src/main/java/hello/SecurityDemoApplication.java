dbe20d3045ac8805e3aabb6ae2c4f95a2eb2f535
BBENCE <bncbodrogi@gmail.com>
2017-01-12 22:44:05 +0100

    security demo app to clarify spring security basics, handling multiple users

---
+++ securityDemo/src/main/java/hello/SecurityDemoApplication.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(SecurityDemoApplication.class, args);
+    }
+}
